c1374a7b906dd8716a98eef0035ee2b0320ef805
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-08-05 19:35:38 +0300

    added secret to env and finished auth module

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(private readonly authService: AuthService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretKey: 'secketKey'
+            secretOrKey: process.env.SECRET_KEY,
         })
     }
 
